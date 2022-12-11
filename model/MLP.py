338ec9c52c14d518af468c5e3778ac1c36bb7e25
GPYINLONG <1434299501@qq.com>
2022-12-11 23:29:34 +0800

    dataset.py renew

---
+++ model/MLP.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+"""
+作者：机智的枫树
+日期：2022年12月11日
+"""
+
+from torch import nn
+
+
+class MLP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.input_layer = nn.Sequential(
+            nn.Linear(784, 256),
+            nn.ReLU(),
+            nn.Dropout(0.2)
+        )
+        self.hidden_layer = nn.Sequential(
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Dropout(0.2)
+        )
+        self.output_layer = nn.Sequential(
+            nn.Linear(256, 10),
+        )
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+
